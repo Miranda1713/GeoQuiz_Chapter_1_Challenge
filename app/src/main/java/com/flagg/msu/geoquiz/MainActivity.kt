5663fe06bf624600bdecf9679d2b979320b717f8
mirandaflagg <mjf846s@missouristate.edu>
2024-02-01 23:27:22 -0600

    Initial Commit

--- app/src/main/java/com/flagg/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/flagg/msu/geoquiz/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,12 +17,26 @@ class MainActivity : AppCompatActivity() {
         trueButton  = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener { view: View ->
+        trueButton.setOnClickListener {
             // Do something in response to the click here
+            val snackBar = Snackbar.make(
+                it,
+                "Correct",
+                Snackbar.LENGTH_SHORT
+            )
+            snackBar.show()
         }
 
-        falseButton.setOnClickListener { view: View ->
+        falseButton.setOnClickListener {
             // Do something in response to the click here
+            val snackBar = Snackbar.make(
+                it,
+                "Incorrect",
+                Snackbar.LENGTH_SHORT
+            )
+            snackBar.show()
         }
     }
-}
+
+
+    }
